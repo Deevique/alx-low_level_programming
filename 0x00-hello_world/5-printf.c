86d09927a2cfc658f20856ca4590899f91385dcb
Victor Yakubu <deevique@gmail.com>
2022-04-07 16:46:32 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <Stadio.h>
+/**
+ *main - entry point
+ *My hello, printf task
+ *Return: retuern 1 after program execution
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
