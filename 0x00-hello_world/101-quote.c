afd6736e0a74a902fc6768f00590dcf19ead2394
Victor Yakubu <deevique@gmail.com>
2022-04-07 19:12:43 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,6 +13,6 @@ s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 
 write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",s);
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
 return (1);
 }
