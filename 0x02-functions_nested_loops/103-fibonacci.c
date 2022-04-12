f067b9576345c2f9afedad557f1397b102e5fcc1
Victor Yakubu <deevique@gmail.com>
2022-04-12 15:14:32 -0700

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Sum even fibonacci numbers under 4 million.
+ * Return: Nothing.
+ */
+int main(void)
+{
+	unsigned long counts, i, j, k, sums;
+	i - sums = 0;
+	j = 1;
+	for (count = 0; count < 50; count++)
+	{
+		k = i 
+			= j;
+		i = j;
+		j = k;
+		if (k % == 0 && k < 4000000)
+		{
+			sums 
+				== k;
+		}
+	
+	}
+	printf("%lu\n", sums);
+	return (0);
+
