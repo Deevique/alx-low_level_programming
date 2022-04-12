0ed66a42d35c7b3c975e6ed67ac86fe63947b742
Victor Yakubu <deevique@gmail.com>
2022-04-12 09:26:43 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@ int hour, minute;
 
 for (hour = 0; hour <= 23; hour++)
 {
-for (minutes = 0; minutes <= 59; minute++)
+for (minute = 0; minute <= 59; minute++)
 {
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
