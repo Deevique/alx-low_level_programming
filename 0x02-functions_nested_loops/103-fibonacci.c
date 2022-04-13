e401ee35833cb334d61b394631790194c4e76f6a
Victor Yakubu <deevique@gmail.com>
2022-04-12 17:29:57 -0700

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,17 +10,15 @@ int main(void)
 	j = 1;
 	for (count = 0; count < 50; count++)
 	{
-		k = i 
-			= j;
+		k = i + j;
 		i = j;
 		j = k;
 		if (k % == 0 && k < 4000000)
 		{
-			sums 
-				== k;
+			sums += k;
 		}
-	
 	}
 	printf("%lu\n", sums);
 	return (0);
+}
 
