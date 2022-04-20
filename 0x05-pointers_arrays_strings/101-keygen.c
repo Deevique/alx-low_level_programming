d9c9896142b4af380f4d470cde8688186863f7d2
Victor Yakubu <deevique@gmail.com>
2022-04-20 09:29:21 -0700

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,29 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /**
+ * main - check the code for the program 101-crackme.
+ * 
+ * Return: Always 0.
  */
 int main(void)
 {
-	int num;
-	srand(time(0));
-	num = rand();
-	printf("%i\n", num);
-	return (0);
+char seed[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char password[22];
+int i, n, passw, complem;
+
+srand(time(0));
+
+for (i = 0; passw < 2772; i++)
+{
+n = rand() % 10;
+password[i] = seed[n];
+passw += password[i];
+}
+complem = 2772 - passw;
+password[i] = complem;
+printf("%s\n",  password);
+return (0);
 }
 
