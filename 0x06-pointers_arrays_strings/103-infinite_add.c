ed10c5caa1db4212e41a8afe2c5d544f88f7a6e4
Victor Yakubu <deevique@gmail.com>
2022-04-24 15:11:04 -0700

    Task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,16 +1,20 @@
-#include "main.h"
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/**
- */
-int main(void)
-{
-	int num;
-
-	srand(time(0));
-	num = rand();
-	printf("%i\n", num);
-	return (0);
+#include<stdio.h>
+ 
+int main() {
+   int num1, num2, res;
+ 
+   printf("\nEnter the two numbers : ");
+   scanf("%d %d", &num1, &num2);
+ 
+   //Call Function Sum With Two Parameters
+   res = sum(num1, num2);
+ 
+   printf("nAddition of two number is : ");
+   return (0);
 }
-
+ 
+int sum(int num1, int num2) {
+   int num3;
+   num3 = num1 + num2;
+   return (num3);
+}
