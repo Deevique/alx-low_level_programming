ad28fcd687d729c59022e2551afeef0e64063692
Victor Yakubu <deevique@gmail.com>
2022-04-25 04:58:37 -0700

    Task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,20 +1,51 @@
-#include<stdio.h>
- 
-int main() {
-   int num1, num2, res;
- 
-   printf("\nEnter the two numbers : ");
-   scanf("%d %d", &num1, &num2);
- 
-   //Call Function Sum With Two Parameters
-   res = sum(num1, num2);
- 
-   printf("nAddition of two number is : ");
-   return (0);
-}
- 
-int sum(int num1, int num2) {
-   int num3;
-   num3 = num1 + num2;
-   return (num3);
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - Prints a buffer
+ * @b: char
+ * @size: int
+ * Return:void
+ */
+void print_buffer(char *b, int size)
+{
+	int x, i;
+
+	for (x = 0; x < size; x += 10)
+	{
+		printf("%08x: ", x);
+
+		for (i = 0; i < 10; i++)
+		{
+			if ((i + x) >= size)
+				printf("  ");
+
+			else
+				printf("%02x", *(b + i + x));
+
+			if ((i % 2) != 0 && i != 0)
+				printf(" ");
+		}
+
+		for (i = 0; i < 10; i++)
+		{
+			if ((i + x) >= size)
+				break;
+
+			else if (*(b + i + x) >= 31 &&
+				 *(b + i + x) <= 126)
+				printf("%c", *(b + i + x));
+
+			else
+				printf(".");
+		}
+
+		if (x >= size)
+			continue;
+
+		printf("\n");
+	}
+
+	if (size <= 0)
+		printf("\n");
 }
