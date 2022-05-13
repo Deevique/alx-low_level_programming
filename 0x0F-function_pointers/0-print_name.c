ccf096c70a2bf78a5a9162d8ef7fbfa65c262865
Victor Yakubu <deevique@gmail.com>
2022-05-13 15:12:29 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,16 @@
 #include "function_pointers.h"
 
 /**
-*print_name - prints a name
-*@name:name to print
-*@f: function which print name
-*/
+ * print_name - prints a name
+ * @name: name's main
+ * @f: void function
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
-
-if (name == NULL || f == NULL)
-return;
-
-f(name);
-
+	if (name && f)
+	{
+		f(name);
+	}
 }
 
