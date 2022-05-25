497a643a572a87cb042fd2fc7745e9a903d85349
Victor Yakubu <deevique@gmail.com>
2022-05-25 06:42:59 -0700

    Task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,9 +1,9 @@
-i#include "lists.h"
+#include "lists.h"
+#include <stdlib.h>
 
 /**
- *free_list - Frees a list
- *@head: A pointer to the list head
- *
+ * free_list - Frees a list_t list.
+ * @head: head pointer
  */
 
 void free_list(list_t *head)
