0ed335b7ef2aeaa650e9e4c700dbf35c7ee4801a
Victor Yakubu <deevique@gmail.com>
2022-05-26 05:18:43 -0700

    Header

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,9 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LIST_H
+#define LIST_H
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 /**
  * struct listint_s - singly linked list
@@ -10,13 +11,19 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ * for Holberton project
  */
+
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
+
+/*Advanced tasks function prototypes stat*/
+listint_t *reverse_listint(listint_t **head);
+
+/* ends */
 
 int _putchar(char);
 size_t print_listint(const listint_t *h);
